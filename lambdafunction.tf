95951701f3f9c743334a0c9ed17608b63b4a34c9
BODA MAHESH KUMAR <93734751+mahi-302@users.noreply.github.com>
2022-01-23 19:36:38 +0530

    Update lambdafunction.tf

--- lambdafunction.tf
+++ lambdafunction.tf
@@ -12,6 +12,6 @@ init_script = """"#!/bin/bash
  sudo apt-get upgrade -y
  sudo apt-get install apache2 -y""""
 def lambda_handler(event, context):
- instance = ec2.run_instances(ImageId=ami-0fb653ca2d3203ac1,InstanceType=t2.micro,KeyName=Ec2all,SubnetId='subnet-0c9f6c09d2f62dc2d,MaxCount=1,MinCount=1,UserData=init_script)
+ instance = ec2.run_instances(ImageId=AMI,InstanceType=INSTANCE_TYPE,KeyName=KEY_NAME,SubnetId=SUBNET_ID,MaxCount=1,MinCount=1,UserData=init_script)
  instance_id = instance['Instances'][0]["i-0ae95d4c23948efa5"]
  return instance_id
